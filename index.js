3c5538cf31053a9e196e45038edbe90b5e471407
BrunoWiedemann <brunin1598@gmail.com>
2019-12-11 22:41:43 -0300

    Correção de bugs, para que a api funcione

--- index.js
+++ index.js
@@ -11,10 +11,10 @@ server.use((req, res, next) => {
 
 function checkCard(req, res, next) {
     const {id} = req.params;
-    const card = cards.find(card => card.id ==id);
+    const card = cards.find(card => card.id == id);
 
-    if(!card){
-        return res.json({error: "Card not found."})
+    if(!card) {
+        return res.json({error: "card not found."});
     }
 
     next();
@@ -40,23 +40,24 @@ server.post("/cards", (req, res) => {
         content
     };
 
-    cards.push(cards);
+    cards.push(card);
 
     nextId++;
-
+    
     return res.json(card);
 });
 
-server.put("/cards/:id",checkCard, (req, res) => {
-    const {title ,content} = req.body;
+server.put("/cards/:id", checkCard, (req, res) => {
+    const {id} = req.params;
+    const {title, content} = req.body;
 
-    const card = cards.find(card => card.id ==id);
+    const card = cards.find(card => card.id == id);
 
-    if(title){
+    if(title) {
         card.title = title;
     }
 
-    if(content){
+    if(content) {
         card.content = content;
     }
 
@@ -64,14 +65,14 @@ server.put("/cards/:id",checkCard, (req, res) => {
 
 });
 
-server.delete("/cards/:id",checkCard, (req, res) =>{
-    const {id} = req.body;
-    const cardIndex = cards.findIndex(card => card.id ==id);
+server.delete("/cards/:id", checkCard, (req, res) => {
+    const {id} = req.params;
+
+    const cardIndex = cards.findIndex(card => card.id == id);
 
     cards.splice(cardIndex, 1);
 
     res.json(cards);
-
 });
 
 server.listen(3333);
